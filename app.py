551a7e099c893cdb1e410924c74b92a79bc7029d
Pavel Naydenov <pavel.snk@gmail.com>
2023-12-10 18:18:25 +0300

    варианты основных страниц

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/log_in')
+def log_in():
+    return render_template('log_in.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/board_office')
+def board_office():
+    return render_template('board_office.html')
+
+
+@app.route('/lk_page')
+def lk_page():
+    return render_template('lk_page.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
