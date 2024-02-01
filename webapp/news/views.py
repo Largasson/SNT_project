bcd6d3979927273f36812d765afb7cc434a0ce78
Pavel Naydenov <pavel.snk@gmail.com>
2024-02-01 13:31:27 +0300

    1. Переделал форму для новостей в файле forms модуля lk. 2. Переделал страницу правления, вставл jinja в форму загрузки новостей. 3. Переделал вьюху кабинета правления для корректной работы загрузки новостей. Устранил конфликт между двумя формами. 4. Дополнил страницу новостей. 5. Дополнил вьюху новостей для корректноого вывода новостей на странице.

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+
+from webapp.news.models import News
 from webapp.weather import get_weather, translate_weather_condition
 
 blueprint = Blueprint('news', __name__)
@@ -10,12 +12,21 @@ def index():
     """ Функция, отвечающая за главную страницу. Передает в функцию
             рендеринга макет главной страницы, информацию по погоде """
     title = 'Главная страница'
+    news = News.query.all()
+    print(news)
+    # try:
+    #     news = News.query.all()
+    # except ValueError:
+    #     print('нет новостей')
+    #     news = []
 
     today_forecast, tomorrow_forecast, weekend_forecast = get_weather()
     today_condition = translate_weather_condition(today_forecast['condition']).capitalize()
     tomorrow_condition = translate_weather_condition(tomorrow_forecast['condition']).capitalize()
     weekend_condition = translate_weather_condition(weekend_forecast['condition']).capitalize()
+
+
     return render_template('news/index.html', page_title=title, today_forecast=today_forecast,
                            tomorrow_forecast=tomorrow_forecast, weekend_forecast=weekend_forecast,
                            today_condition=today_condition, tomorrow_condition=tomorrow_condition,
-                           weekend_condition=weekend_condition)
+                           weekend_condition=weekend_condition, news=news)
