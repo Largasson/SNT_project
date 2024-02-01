0ac2a7b8c1dc8ab2dec549d35f4f50e1ca8a5747
Mikhail Turilov <turilovm@gmail.com>
2024-02-01 08:14:57 +0300

    mod admin page_1

--- webapp/lk/forms.py
+++ webapp/lk/forms.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, StringField, TextAreaField
+from wtforms.validators import DataRequired
 
 
 class UploadFileForm(FlaskForm):
@@ -11,3 +12,10 @@ class UploadFileForm(FlaskForm):
         f = self.file.data
         text_from_csv = f.read().decode('cp1251')
         return StringIO(text_from_csv)
+
+
+
+class NewsForm(FlaskForm):
+    news_title = StringField('Заголовок новости', validators=[DataRequired()])
+    news_content = TextAreaField('Содержание новости', render_kw={'class': 'btn btn-primary'})
+    submit = SubmitField('Опубликовать')
--- webapp/lk/views.py
+++ webapp/lk/views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user
+
+from webapp import db
+from webapp.news.models import News
 from webapp.user.forms import LoginForm
 from webapp.lk.models import FinancialData
-from webapp.lk.forms import UploadFileForm
+from webapp.lk.forms import UploadFileForm, NewsForm
 from webapp.parsing_csv import parsing_csv
 from webapp.loader import insert_finance_data_db
 from logging import basicConfig, info, INFO
+from datetime import datetime
 
 basicConfig(filename='pars_log.log', level=INFO, format="%(asctime)s %(levelname)s %(message)s")
 
@@ -36,6 +40,7 @@ def board_office():
       ФОРМУ загрузки файла, а также макет админ-страницы. Обрабатывает приходящий файл  """
     if current_user.is_admin:
         form = UploadFileForm()
+        news_form = NewsForm()
         title = 'Страница Правления'
         if form.validate_on_submit():
             csv_file = form.convert_file_field_data_to_csv_file()
@@ -46,5 +51,14 @@ def board_office():
             for k in key_sort:
                 info(f'КЛЮЧ {k}: {values_to_db[k]}')
             return render_template('lk/board_office.html', a=form, page_title=title)
+        elif request.method == 'POST':
+            news_title = news_form.news_title.data
+            news_content = news_form.news_content.data
+            new_news = News(published=datetime.utcnow(), text=news_content, title=news_title)
+            db.session.add(new_news)
+            db.session.commit()
+            return redirect(url_for('lk.board_office'))
+        news_items = News.query.all()
+        return render_template('index.html', news_items=news_items)
         return render_template('lk/board_office.html', a=form, page_title=title)
     return redirect(url_for('lk.lk_page', area=current_user.area_number))
